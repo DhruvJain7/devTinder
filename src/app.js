edfb7f98f64b31a2e36caca9e64970633e5ce332
Dhruv Jain <djmjtj10@gmail.com>
2025-12-30 15:07:04 +0530

    Test-Apis added

--- src/app.js
+++ src/app.js
@@ -3,16 +3,21 @@ const express = require("express");
 const app = express();
 const { connectDb } = require("./config/database");
 const User = require("./models/user.js");
+app.use(express.json());
+// get user by email
+app.get("/email", async (req, res) => {
+  const userEmail = req.body.emailId;
 
+  try {
+    await User.find({ emailId: userEmail });
+    res.send(userEmail);
+  } catch {
+    res.status(400).send("Something went wrong!");
+  }
+});
 //Post - User SignUp
 app.post("/signup", async (req, res) => {
-  const userObj = {
-    firstName: "Virat",
-    lastName: "Kohli",
-    emailId: "viratKohli@gmail.com",
-    password: "ViratKohli@123",
-  };
-  const user = new User(userObj);
+  const user = new User(req.body);
   try {
     await user.save();
     res.send("User Added successfully");
@@ -20,7 +25,15 @@ app.post("/signup", async (req, res) => {
     res.status(400).send("Error Saving the user:" + err.message);
   }
 });
-
+// get - all profiles feed
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
 connectDb()
   .then(() => {
     console.log("Database Connection Established");
