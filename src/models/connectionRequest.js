14599b5a286b4c386f5059bbe2463e08c9ae7fc0
Dhruv Jain <djmjtj10@gmail.com>
2026-01-10 21:55:59 +0530

    Completed Send Connection Request

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -13,7 +13,7 @@ const connectionRequestSchema = new mongoose.Schema(
     status: {
       type: String,
       enum: {
-        values: ["ignore", "interested", "accepted", "rejected"],
+        values: ["ignored", "interested", "accepted", "rejected"],
         message: `{VALUE} is incorrect status type`,
       },
       required: true,
@@ -23,4 +23,15 @@ const connectionRequestSchema = new mongoose.Schema(
     timestamps: true,
   },
 );
+
+connectionRequestSchema.index({ fromUserId: 1, toUserId: 1 });
+
+connectionRequestSchema.pre("save", function (next) {
+  const connectionRequest = this;
+  // Check if the fromUserId is same as toUserId
+  if (connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
+    throw new Error("Cannot send connection request to yourself!");
+  }
+  next();
+});
 module.exports = mongoose.model("ConnectionRequest", connectionRequestSchema);
