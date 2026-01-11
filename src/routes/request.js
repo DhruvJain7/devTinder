9c184ea6723ee55a80db84b40606732be1b39885
Dhruv Jain <djmjtj10@gmail.com>
2026-01-11 20:04:46 +0530

    Added review Connection Request

--- src/routes/request.js
+++ src/routes/request.js
@@ -25,6 +25,11 @@ requestRouter.post(
       if (!toUser) {
         return res.status(404).json({ message: "User not found!!" });
       }
+      if (fromUserId.toString() === toUserId.toString()) {
+        return res
+          .status(400)
+          .json({ message: "Cannot send connection request to yourself!" });
+      }
 
       const existingConnectionRequest = await ConnectionRequest.findOne({
         $or: [
@@ -51,6 +56,37 @@ requestRouter.post(
       });
     } catch (err) {
       res.status(400).send("Error:" + err.message);
+    }
+  },
+);
+requestRouter.post(
+  "/request/review/:status/:requestId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const loggedInUser = req.user;
+      const { status, requestId } = req.params;
+      const allowedStatus = ["accepted", "rejected"];
+      if (!allowedStatus.includes(status)) {
+        return res.status(400).json({
+          message: "Status not allowed!",
+        });
+      }
+      const connectionRequest = await ConnectionRequest.findOne({
+        _id: requestId,
+        toUserId: loggedInUser._id,
+        status: "interested",
+      });
+      if (!connectionRequest) {
+        return res
+          .status(404)
+          .json({ message: "Connection Request not found" });
+      }
+      connectionRequest.status = status;
+      const data = await connectionRequest.save();
+      res.json({ message: "Connection request updated", data });
+    } catch (err) {
+      res.status(400).send("Error" + err.message);
     }
   },
 );
